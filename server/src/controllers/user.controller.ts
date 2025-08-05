3d443048bbe058cf3c977829ab847f4fe173bc9b
s3nsh1-dev <shubham228pandey@gmail.com>
2025-08-05 23:57:24 +0530

    cloudinary Uplading first phase

--- server/src/controllers/user.controller.ts
+++ server/src/controllers/user.controller.ts
@@ -1,6 +1,7 @@
 import { asyncHandler } from "../utils/asyncHandler";
 import ApiError from "../utils/ApiError";
 import { User } from "../models/user.model";
+import { uploadOnCloudinary } from "../utils/cloudinary";
 
 const registerUser = asyncHandler(async (req, res) => {
   const { fullName, email, username, password } = req.body;
@@ -16,8 +17,20 @@ const registerUser = asyncHandler(async (req, res) => {
   if (exitedUser)
     throw new ApiError(409, "User with email or username already exist");
 
+  // read about this type casting
   const files = req.files as { [fieldname: string]: Express.Multer.File[] };
-  const avatarPath = files.avatar?.[0]?.path;
+  const avatarLocalPath = files.avatar[0]?.path;
+  const coverImagePath = files.coverImage[0]?.path;
+
+  if (!avatarLocalPath) {
+    throw new ApiError(404, "Avatar not uploaded correctly to multer");
+  }
+  const checkAvatarCloudinaryUpload = await uploadOnCloudinary(avatarLocalPath);
+
+  if (!coverImagePath)
+    throw new ApiError(404, "CoverImage not Uploaded Correctly to multer");
+  const checkCoverImageCloudinaryUpload =
+    await uploadOnCloudinary(coverImagePath);
 });
 
 export { registerUser };
