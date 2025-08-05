135bfad7b17406842120b353fdf90c85586f02ea
s3nsh1-dev <shubham228pandey@gmail.com>
2025-08-05 23:37:41 +0530

    Validation and calling

--- server/src/controllers/user.controller.ts
+++ server/src/controllers/user.controller.ts
@@ -1,7 +1,15 @@
 import { asyncHandler } from "../utils/asyncHandler";
+import ApiError from "../utils/ApiError";
+import { User } from "../models/user.model";
 
 const registerUser = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "OK" });
+  const { fullName, email, username, password } = req.body;
+  // console.log("email: ", email);
+  if (
+    [fullName, email, username, password].some((field) => field.trim() === "")
+  ) {
+    throw new ApiError(400, "fill all required field..!");
+  }
 });
 
 export { registerUser };
--- server/src/routes/user.routes.ts
+++ server/src/routes/user.routes.ts
@@ -1,8 +1,15 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/user.controller";
+import { multerUpload } from "../middleware/multer.middleware";
 
 const userRouter = Router();
 
-userRouter.route("/register").post(registerUser);
+userRouter.route("/register").post(
+  multerUpload.fields([
+    { name: "avatar", maxCount: 1 },
+    { name: "coverImage", maxCount: 1 },
+  ]),
+  registerUser
+);
 
 export default userRouter;
