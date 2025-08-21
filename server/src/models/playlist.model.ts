1252204ed6e19a21ff772d05cd7403e53700890f
s3nsh1-dev <shubham228pandey@gmail.com>
2025-08-21 23:53:37 +0530

    all project related model created

--- server/src/models/playlist.model.ts
+++ server/src/models/playlist.model.ts
@@ -1,6 +1,25 @@
 import mongoose from "mongoose";
 
-const schema = {};
+const schema = {
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  videos: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Video",
+    },
+  ],
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+};
 const playlistSchema = new mongoose.Schema(schema, { timestamps: true });
 
 export const Playlist = mongoose.model("Playlist", playlistSchema);
