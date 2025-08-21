483f3a66976b08125b99c9ecf816d2933b013f0e
s3nsh1-dev <shubham228pandey@gmail.com>
2025-08-21 23:53:37 +0530

    all project related model created

--- server/src/models/comment.model.ts
+++ server/src/models/comment.model.ts
@@ -1,6 +1,20 @@
-import mongoose from "mongoose";
+import mongoose, { Schema } from "mongoose";
 
-const schema = {};
+const schema = {
+  content: {
+    type: String,
+    required: true,
+  },
+  video: {
+    type: Schema.Types.ObjectId,
+    ref: "Video",
+  },
+  owner: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+};
+
 const commentSchema = new mongoose.Schema(schema, { timestamps: true });
 
 export const Comment = mongoose.model("Comment", commentSchema);
--- server/src/models/tweet.model.ts
+++ server/src/models/tweet.model.ts
@@ -1,6 +1,15 @@
 import mongoose from "mongoose";
 
-const schema = {};
+const schema = {
+  content: {
+    type: String,
+    required: true,
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+};
 const tweetSchema = new mongoose.Schema(schema, { timestamps: true });
 
 export const Tweet = mongoose.model("Tweet", tweetSchema);
