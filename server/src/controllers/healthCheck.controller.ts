a21bf4eb6bb749c1ca42561a2beba758facc63bd
s3nsh1-dev <shubham228pandey@gmail.com>
2025-08-22 23:52:19 +0530

    EOD22Aug

--- server/src/controllers/healthCheck.controller.ts
+++ server/src/controllers/healthCheck.controller.ts
@@ -1,6 +1,6 @@
-import { ApiError } from "../utils/ApiError.js";
-import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
+import ApiError from "../utils/ApiError.js";
+import ApiResponse from "../utils/ApiResponse.js";
 
 const healthCheck = asyncHandler(async (req, res) => {
   //TODO: build a healthCheck response that simply returns the OK status as json with a message
---
+++ server/src/routes/video.route.ts
@@ -0,0 +1,41 @@
+import { Router } from "express";
+import {
+  deleteVideo,
+  getAllVideos,
+  getVideoById,
+  publishAVideo,
+  togglePublishStatus,
+  updateVideo,
+} from "../controllers/video.controller.js";
+import verifyJWT from "../middleware/auth.middleware.js";
+import { multerUpload } from "../middleware/multer.middleware.js";
+
+const videoRouter = Router();
+videoRouter.use(verifyJWT); // Apply verifyJWT middleware to all routes in this file
+
+videoRouter
+  .route("/")
+  .get(getAllVideos)
+  .post(
+    multerUpload.fields([
+      {
+        name: "videoFile",
+        maxCount: 1,
+      },
+      {
+        name: "thumbnail",
+        maxCount: 1,
+      },
+    ]),
+    publishAVideo
+  );
+
+videoRouter
+  .route("/:videoId")
+  .get(getVideoById)
+  .delete(deleteVideo)
+  .patch(multerUpload.single("thumbnail"), updateVideo);
+
+videoRouter.route("/toggle/publish/:videoId").patch(togglePublishStatus);
+
+export default videoRouter;
