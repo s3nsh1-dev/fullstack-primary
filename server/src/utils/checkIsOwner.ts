41b53eea7ecf8bb038b571cf048fc8dd3a0fe2ff
s3nsh1-dev <shubham228pandey@gmail.com>
2025-08-24 23:08:01 +0530

    Implement video ownership checks and enhance video update/delete functionality

--- server/src/utils/checkIsOwner.ts
+++ server/src/utils/checkIsOwner.ts
@@ -1,8 +1,14 @@
-// to check weather the owner _id and the _id we get from req.user is same or notDeepEqual// utils/ownership.ts
+// to check whether the owner _id and the _id we get from req.user is same or not
 
 import { Types } from "mongoose";
 
 export const isOwner = (
   owner: Types.ObjectId,
   userId: string | Types.ObjectId
-) => owner.toString() === String(userId);
+) => {
+  try {
+    return owner.toString() === String(userId);
+  } catch {
+    throw new Error("INVALID OWNER ID OR USER ID");
+  }
+};
