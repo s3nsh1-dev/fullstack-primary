f86e701fa19bd9faa746b19f961d9ff065d4e5bc
s3nsh1-dev <shubham228pandey@gmail.com>
2025-08-28 05:24:47 +0530

    health Check immplemented

--- server/src/routes/healthcheck.route.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { healthCheck } from "../controllers/healthCheck.controller";
-
-const healthCheckRouter = Router();
-
-healthCheckRouter.route("/").get(healthCheck);
-
-export default healthCheckRouter;
