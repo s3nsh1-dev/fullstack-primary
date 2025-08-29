0c923f4ce524ad77249cb8743e4b8dafe9856dad
s3nsh1-dev <shubham228pandey@gmail.com>
2025-08-29 22:54:36 +0530

    toggle subscription completed

--- server/src/controllers/subscription.controller.ts
+++ server/src/controllers/subscription.controller.ts
@@ -6,17 +6,52 @@ import ApiError from "../utils/ApiError";
 import ApiResponse from "../utils/ApiResponse";
 
 const toggleSubscription = asyncHandler(async (req, res) => {
-  const { channelId } = req.params;
   // TODO: toggle subscription
+
+  const { channelId } = req.params;
+  if (!req.user || !req.user._id) throw new ApiError(401, "UNAUTHORIZED USER");
+  const subscriberId = req.user._id;
+  if (!isValidObjectId(channelId) || !isValidObjectId(subscriberId)) {
+    throw new ApiError(400, "INVALID CHANNEL_ID OR SUBSCRIBER_ID");
+  }
+
+  let resultFindings;
+  const findChannelByID = await Subscription.findOne({
+    channel: channelId,
+    subscriber: subscriberId,
+  });
+  if (!findChannelByID) {
+    const createSubDocument = await Subscription.create({
+      channel: channelId,
+      subscriber: subscriberId,
+    });
+    if (!createSubDocument)
+      throw new ApiError(500, "SUBSCRIPTION CREATION FAILED");
+    resultFindings = createSubDocument;
+  } else {
+    resultFindings = await Subscription.findByIdAndDelete(findChannelByID._id);
+  }
+
+  res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        { result: resultFindings },
+        "Successfully Subscription Toggled"
+      )
+    );
 });
 
-// controller to return subscriber list of a channel
 const getUserChannelSubscribers = asyncHandler(async (req, res) => {
+  // TODO: controller to return subscriber list of a channel
+
   const { channelId } = req.params;
 });
 
-// controller to return channel list to which user has subscribed
 const getSubscribedChannels = asyncHandler(async (req, res) => {
+  // TODO: controller to return channel list to which user has subscribed
+
   const { subscriberId } = req.params;
 });
 
