687e5fafba9a7c7fcf0a43f4762a23ac8497d11a
s3nsh1-dev <shubham228pandey@gmail.com>
2025-08-29 22:54:36 +0530

    toggle subscription completed

--- server/src/models/subscription.model.ts
+++ server/src/models/subscription.model.ts
@@ -1,5 +1,11 @@
 import mongoose from "mongoose";
 
+/*
+User_A is subscribing to User_B ==then==>  A is subscriber and B is channel
+so if i want to Unsubscribe User_B(Channel) from User_A(Subscriber).
+I have to delete the document where subscriber is A and channel is B.
+*/
+
 const schema = {
   subscriber: {
     type: mongoose.Schema.Types.ObjectId,
