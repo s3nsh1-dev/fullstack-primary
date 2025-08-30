fffd2602870895a07d0ae2ea7e4f3ccc02a39762
s3nsh1-dev <shubham228pandey@gmail.com>
2025-08-30 19:59:10 +0530

    first iteration of comment controller

--- server/src/controllers/comment.controller.ts
+++ server/src/controllers/comment.controller.ts
@@ -6,20 +6,111 @@ import { asyncHandler } from "../utils/asyncHandler";
 
 const getVideoComments = asyncHandler(async (req, res) => {
   //TODO: get all comments for a video
+
   const { videoId } = req.params;
   const { page = 1, limit = 10 } = req.query;
+
+  const videoComments = await Comment.aggregate([
+    { $match: { video: videoId } },
+    {
+      $lookup: {
+        from: "user",
+        as: "ownerDetails",
+        foreignField: "_id",
+        localField: "owner",
+        pipeline: [
+          {
+            $project: {
+              _id: 1,
+              fullname: 1,
+              avatar: 1,
+            },
+          },
+        ],
+      },
+    },
+    { $skip: Number((+page - 1) * +limit) },
+  ]);
+  if (!videoComments) {
+    throw new ApiError(400, "COMMENTS NOT FOUND");
+  }
+
+  res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        { comments: videoComments },
+        "COMMENTS FETCHED SUCCESSFULLY"
+      )
+    );
 });
 
 const addComment = asyncHandler(async (req, res) => {
-  // TODO: add a comment to a video
+  // TODO: add a comment to a video, how its using populate in create ?
+
+  const { video_ID } = req.params;
+  const { content } = req.body;
+  if (!req.user || !req.user._id) throw new ApiError(400, "UNAUTHORIZED USER");
+
+  const comment = await Comment.create({
+    content,
+    video: new mongoose.Schema.Types.ObjectId(video_ID),
+    owner: new mongoose.Schema.Types.ObjectId(String(req.user._id)),
+  });
+  if (!comment) throw new ApiError(400, "COMMENT NOT REGISTERED");
+
+  res
+    .status(200)
+    .json(new ApiResponse(200, { comment }, "COMMENT REGISTER SUCCESSFULLY"));
 });
 
 const updateComment = asyncHandler(async (req, res) => {
   // TODO: update a comment
+
+  const { comment_ID } = req.params;
+  const { content } = req.body;
+
+  const updateComment = await Comment.findByIdAndUpdate(
+    comment_ID,
+    { $set: { content } },
+    { new: true }
+  )
+    .populate("owner", " _id avatar fullname")
+    .populate("video", "_id title");
+  if (!updateComment) throw new ApiError(400, "COMMENT NOT UPDATED");
+
+  res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        { result: updateComment },
+        "COMMENT UPDATED SUCCESSFULLY"
+      )
+    );
 });
 
 const deleteComment = asyncHandler(async (req, res) => {
   // TODO: delete a comment
+
+  const { comment_ID } = req.params;
+  if (!comment_ID) throw new ApiError(400, "COMMENT NOT FOUND");
+
+  const deleteComment = await Comment.findByIdAndDelete(comment_ID)
+    .populate("owner", "_id fullname")
+    .populate("video", "_id title");
+  if (!deleteComment) throw new ApiError(404, "COMMENT NOT FOUND");
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        { result: deleteComment },
+        "Comment deleted successfully"
+      )
+    );
 });
 
 export { getVideoComments, addComment, updateComment, deleteComment };
