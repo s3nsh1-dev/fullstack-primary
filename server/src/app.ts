d25ce83831dcb810384545249155684bf89ce70b
s3nsh1-dev <shubham228pandey@gmail.com>
2025-09-01 21:05:00 +0530

    SOD

--- server/src/app.ts
+++ server/src/app.ts
@@ -8,6 +8,7 @@ import videoRouter from "./routes/video.route";
 import healthCheckRouter from "./routes/healthCheck.route";
 import tweetRouter from "./routes/tweet.route";
 import subscriptionRouter from "./routes/subscription.route";
+import commentRouter from "./routes/comment.route";
 
 const app = express();
 /**
@@ -47,5 +48,6 @@ app.use("/api/v1/videos", videoRouter);
 app.use("/api/v1/health", healthCheckRouter);
 app.use("/api/v1/tweets", tweetRouter);
 app.use("/api/v1/subscriptions", subscriptionRouter);
+app.use("/api/v1/comments", commentRouter);
 
 export { app };
