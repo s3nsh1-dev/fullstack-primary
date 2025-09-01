60f2ffed0c9183846d81508978a8b93a549cf63f
s3nsh1-dev <shubham228pandey@gmail.com>
2025-09-01 23:59:31 +0530

    Add like functionality and subscription ownership check
    - Implemented like feature for videos, including creation and deletion of likes.
    - Added validation for user authentication and video ID.
    - Enhanced subscription toggle logic with ownership verification.

--- server/src/controllers/like.controller.ts
+++ server/src/controllers/like.controller.ts
@@ -5,18 +5,68 @@ import ApiError from "../utils/ApiError";
 import ApiResponse from "../utils/ApiResponse";
 
 const toggleVideoLike = asyncHandler(async (req, res) => {
-  const { videoId } = req.params;
   //TODO: toggle like on video
+
+  const { videoId } = req.params;
+  let responseMessage = "";
+  let resultFindings;
+
+  if (!isValidObjectId(videoId))
+    throw new ApiError(400, "OBJECT IN PARAMETER IS NOT VALID");
+  if (!req.user || !req.user._id)
+    throw new ApiError(400, "UNAUTHENTICATED USER");
+
+  // searching with videoId and user only give verified document
+  const searchLike = await Like.findOne({
+    video: videoId,
+    likedBy: req.user._id,
+  });
+  if (!searchLike) {
+    // No like then create like
+    const createLike = await Like.create({
+      video: videoId,
+      likedBy: req.user._id.toString(),
+    });
+    if (!createLike) throw new ApiError(400, "LIKE CREATION FAILED");
+    resultFindings = createLike;
+    responseMessage = "LIKE ADDED TO VIDEO";
+  } else {
+    resultFindings = await Like.findOneAndDelete({
+      video: videoId,
+      likedBy: req.user._id,
+    });
+    responseMessage = "LIKE DOCUMENT DELETED";
+  }
+
+  console.log("did we found LIKEd ?", searchLike);
+
+  res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        { result: resultFindings },
+        `${responseMessage} SUCCESSFULLY`
+      )
+    );
 });
 
 const toggleCommentLike = asyncHandler(async (req, res) => {
-  const { commentId } = req.params;
   //TODO: toggle like on comment
+
+  const { commentId } = req.params;
+  if (!isValidObjectId(commentId)) {
+    throw new ApiError(400, "OBJECT IN PARAMETER IS NOT VALID");
+  }
 });
 
 const toggleTweetLike = asyncHandler(async (req, res) => {
-  const { tweetId } = req.params;
   //TODO: toggle like on tweet
+
+  const { tweetId } = req.params;
+  if (!isValidObjectId(tweetId)) {
+    throw new ApiError(400, "OBJECT IN PARAMETER IS NOT VALID");
+  }
 });
 
 const getLikedVideos = asyncHandler(async (req, res) => {
--- server/src/controllers/subscription.controller.ts
+++ server/src/controllers/subscription.controller.ts
@@ -5,18 +5,21 @@ import { asyncHandler } from "../utils/asyncHandler";
 import ApiError from "../utils/ApiError";
 import ApiResponse from "../utils/ApiResponse";
 import { toObjectId } from "../utils/convertToObjectId";
+import { isOwner } from "../utils/checkIsOwner";
 
 const toggleSubscription = asyncHandler(async (req, res) => {
   // TODO: toggle subscription
 
   const { channelId } = req.params;
+  let responseMessage = "";
+  let resultFindings;
+
   if (!req.user || !req.user._id) throw new ApiError(401, "UNAUTHORIZED USER");
   const subscriberId = req.user._id;
   if (!isValidObjectId(channelId) || !isValidObjectId(subscriberId)) {
     throw new ApiError(400, "INVALID CHANNEL_ID OR SUBSCRIBER_ID");
   }
-  let responseMessage = "";
-  let resultFindings;
+
   const findChannelByID = await Subscription.findOne({
     channel: channelId,
     subscriber: subscriberId,
@@ -31,6 +34,9 @@ const toggleSubscription = asyncHandler(async (req, res) => {
     resultFindings = createSubDocument;
     responseMessage = "SUBSCRIPTION CREATED";
   } else {
+    if (!isOwner(findChannelByID.subscriber, req.user._id.toString())) {
+      throw new ApiError(400, " USER NOT AUTHORIZED TO MAKE CHANGES");
+    }
     resultFindings = await Subscription.findByIdAndDelete(findChannelByID._id);
     responseMessage = "SUBSCRIPTION REMOVED";
   }
