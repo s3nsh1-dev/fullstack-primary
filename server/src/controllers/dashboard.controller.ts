3c0dd70375e79364fa53fdd9e863fd632529f1e8
s3nsh1-dev <shubham228pandey@gmail.com>
2025-09-03 12:02:13 +0530

    videos Stats for user fetched and tested
    going gym will resume after coming back

--- server/src/controllers/dashboard.controller.ts
+++ server/src/controllers/dashboard.controller.ts
@@ -5,13 +5,66 @@ import { Like } from "../models/like.model";
 import { asyncHandler } from "../utils/asyncHandler";
 import ApiError from "../utils/ApiError";
 import ApiResponse from "../utils/ApiResponse";
+import { toObjectId } from "../utils/convertToObjectId";
 
 const getChannelStats = asyncHandler(async (req, res) => {
   // TODO: Get the channel stats like total video views, total subscribers, total videos, total likes etc.
+
+  if (!req.user || !req.user._id)
+    throw new ApiError(400, "UNAUTHENTICATED REQUEST");
 });
 
 const getChannelVideos = asyncHandler(async (req, res) => {
   // TODO: Get all the videos uploaded by the channel
+
+  if (!req.user || !req.user._id)
+    throw new ApiError(400, "UNAUTHENTICATED REQUEST");
+
+  const videos = await Video.aggregate([
+    { $match: { owner: toObjectId(req.user._id.toString()) } },
+    {
+      $lookup: {
+        from: "users",
+        localField: "owner",
+        foreignField: "_id",
+        as: "ownerDetails",
+        pipeline: [
+          {
+            $project: {
+              _id: 1,
+              avatar: 1,
+              username: 1,
+              fullname: 1,
+            },
+          },
+        ],
+      },
+    },
+    {
+      $addFields: {
+        ownerDetails: { $first: "$ownerDetails" },
+      },
+    },
+    {
+      $project: {
+        _id: 1,
+        videoFile: 1,
+        thumbnail: 1,
+        title: 1,
+        description: 1,
+        duration: 1,
+        views: 1,
+        isPublished: 1,
+        createdAt: 1,
+        ownerDetails: 1,
+      },
+    },
+  ]);
+  if (!videos) throw new ApiError(400, "VIDEOS NOT FETCHED");
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, { videos }, "VIDEOS FETCHED SUCCESSFULLY"));
 });
 
 export { getChannelStats, getChannelVideos };
