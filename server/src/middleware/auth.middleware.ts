a4834723e502a4cc92c78ae6190bc790daafac73
s3nsh1-dev <shubham228pandey@gmail.com>
2025-09-05 00:22:30 +0530

    backened to some extend completed

--- server/src/middleware/auth.middleware.ts
+++ server/src/middleware/auth.middleware.ts
@@ -4,7 +4,7 @@ import jwt from "jsonwebtoken";
 import { User } from "../models/user.model";
 import env from "../utils/dotenvHelper";
 import type { UserThisType } from "../constants/ModelTypes";
-import isUserAccessTokenPayloadType from "../utils/checkPayloadType";
+import isAccessPayloadCorrect from "../utils/checkPayloadType";
 
 // Extend Express Request interface to include 'user'
 declare global {
@@ -26,21 +26,27 @@ const verifyJWT = asyncHandler(async (req, _, next) => {
     if (!token) throw new ApiError(401, "UNAUTHENTICATED REQUEST");
 
     const decodedToken = jwt.verify(token, env.ACCESS_TOKEN_SECRET);
-    if (!isUserAccessTokenPayloadType(decodedToken))
-      throw new ApiError(400, "INVALID TOKEN");
 
     let user = null;
-    if (typeof decodedToken === "object" && "_id" in decodedToken) {
+    if (
+      !isAccessPayloadCorrect(decodedToken) &&
+      typeof decodedToken === "object" &&
+      "_id" in decodedToken
+    ) {
       user = await User.findById((decodedToken as jwt.JwtPayload)._id).select(
         "-password -refreshToken"
       );
+    } else {
+      throw new ApiError(400, "INVALID TOKEN");
     }
-    if (!user) throw new ApiError(401, "INVALID ACCESS TOKEN");
 
+    if (!user) throw new ApiError(401, "INVALID ACCESS TOKEN");
     req.user = user;
+
     next();
   } catch (error: any) {
     throw new ApiError(401, error?.message || "INVALID ACCESS TOKEN");
   }
 });
+
 export default verifyJWT;
