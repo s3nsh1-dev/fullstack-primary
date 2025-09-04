8c8e52238472a7c79fcc0e6c268dd8b214db6ab0
s3nsh1-dev <shubham228pandey@gmail.com>
2025-09-04 23:05:33 +0530

    coding for  totak likes

--- server/src/controllers/dashboard.controller.ts
+++ server/src/controllers/dashboard.controller.ts
@@ -7,18 +7,94 @@ import ApiError from "../utils/ApiError";
 import ApiResponse from "../utils/ApiResponse";
 import { toObjectId } from "../utils/convertToObjectId";
 
+type Stats = {
+  [key: string]: string;
+  // Record<string, number>
+};
+
 const getChannelStats = asyncHandler(async (req, res) => {
   // TODO: Get the channel stats like total video views, total subscribers, total videos, total likes etc.
 
   if (!req.user || !req.user._id)
     throw new ApiError(400, "UNAUTHENTICATED REQUEST");
 
-  const totalVideos = await Video.find({ owner: req.user._id });
-  if (!totalVideos) throw new ApiError(400, "ERROR WHILE FETCHING THE VIDEOS");
+  const user_ID = req.user._id;
+  const stats: Stats = {};
+  let totalViews = 0;
 
+  const totalVideos = await Video.find({ owner: user_ID });
+  if (!totalVideos) throw new ApiError(400, "ERROR WHILE FETCHING THE VIDEOS");
+  totalVideos.forEach((item) => {
+    console.log(item.title);
+    totalViews += item.views;
+  });
+
+  const subscribers = await Subscription.find({ channel: user_ID });
+  if (!subscribers) throw new ApiError(400, "CHANNEL NOT FOUND");
+
+  const likes = await Like.aggregate([
+    {
+      $lookup: {
+        from: "videos",
+        localField: "video",
+        foreignField: "_id",
+        as: "videoDetails",
+        pipeline: [
+          {
+            $project: {
+              _id: 1,
+              title: 1,
+              owner: 1,
+            },
+          },
+        ],
+      },
+    },
+    {
+      $lookup: {
+        from: "tweets",
+        localField: "tweet",
+        foreignField: "_id",
+        as: "tweetDetails",
+        pipeline: [
+          {
+            $project: {
+              _id: 1,
+              content: 1,
+              owner: 1,
+            },
+          },
+        ],
+      },
+    },
+    { $unwind: { path: "$videoDetails", preserveNullAndEmptyArrays: true } },
+    { $unwind: { path: "$tweetDetails", preserveNullAndEmptyArrays: true } },
+    {
+      $match: {
+        $or: [
+          { "videoDetails.owner": user_ID },
+          { "tweetDetails.owner": user_ID },
+        ],
+      },
+    },
+    // {
+    //   $project: {
+    //     _id: 1,
+    //     likedBy: 1,
+    //     videoDetails: 1,
+    //     tweetDetails: 1,
+    //   },
+    // },
+  ]);
+
+  stats["totalViews"] = totalViews.toString();
+  stats["totalSubscribers"] = subscribers.length.toString();
+  stats["uploadedVideoCount"] = totalVideos.length.toString();
+
+  console.log(stats);
   return res
     .status(200)
-    .json(new ApiResponse(200, { result: totalVideos }, "HERE IS YOUR RESULT"));
+    .json(new ApiResponse(200, { result: likes }, "HERE IS YOUR RESULT"));
 });
 
 const getChannelVideos = asyncHandler(async (req, res) => {
