9f66d357058cba2190abb391a8b82fab03c145e6
s3nsh1-dev <shubham228pandey@gmail.com>
2025-09-05 20:53:13 +0530

    Not using react compiler
    inorder to go indepth for react rendering issues and optimization i need to master userMemo, usecallback and memo

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -1,18 +1,20 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { globalIgnores } from 'eslint/config'
+// OLD LINT CODE
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
+import { globalIgnores } from "eslint/config";
+import { defineConfig } from "eslint/config";
 
 export default tseslint.config([
-  globalIgnores(['dist']),
+  globalIgnores(["dist"]),
   {
-    files: ['**/*.{ts,tsx}'],
+    files: ["**/*.{ts,tsx}"],
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
+      reactHooks.configs["recommended-latest"],
       reactRefresh.configs.vite,
     ],
     languageOptions: {
@@ -20,4 +22,50 @@ export default tseslint.config([
       globals: globals.browser,
     },
   },
-])
+]);
+
+// NEW LINT CODE
+// import js from "@eslint/js";
+// import globals from "globals";
+// import reactHooks from "eslint-plugin-react-hooks";
+// import reactRefresh from "eslint-plugin-react-refresh";
+// import tseslint from "typescript-eslint";
+// import { defineConfig } from "eslint";
+// import reactCompiler from "eslint-plugin-react-compiler";
+
+// export default defineConfig({
+//   ignores: ["dist/**"],
+//   files: ["**/*.{ts,tsx}"],
+//   extends: [
+//     js.configs.recommended,
+//     tseslint.configs.recommended,
+//     reactHooks.configs["recommended-latest"],
+//     reactRefresh.configs.vite,
+//     reactCompiler.configs.recommended,
+//   ],
+//   languageOptions: {
+//     ecmaVersion: 2020,
+//     globals: {
+//       ...globals.browser,
+//     },
+//     parser: tseslint.parser,
+//     parserOptions: {
+//       project: "./tsconfig.json",
+//       ecmaFeatures: {
+//         jsx: true,
+//       },
+//     },
+//   },
+//   plugins: {
+//     "@typescript-eslint": tseslint.plugin,
+//     "react-hooks": reactHooks,
+//     "react-refresh": reactRefresh,
+//     react: reactPlugin,
+//     "react-compiler": reactCompiler,
+//   },
+//   settings: {
+//     react: {
+//       version: "detect",
+//     },
+//   },
+// });
---
+++ client/src/pages/Dashboard.tsx
@@ -0,0 +1,7 @@
+import { Box } from "@mui/material";
+
+const Dashboard = () => {
+  return <Box>This is Dashboard</Box>;
+};
+
+export default Dashboard;
