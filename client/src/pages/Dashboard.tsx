0b6997a24523d55ff70d893bad6617c9bb6d54e8
s3nsh1-dev <shubham228pandey@gmail.com>
2025-09-06 19:53:41 +0530

    mode working
    light and dark mode are working

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -1,7 +1,16 @@
-import { Box } from "@mui/material";
+import { Box, Button } from "@mui/material";
+import useMode from "../hooks/useMode";
 
 const Dashboard = () => {
-  return <Box>This is Dashboard</Box>;
+  const { changeMode } = useMode();
+  return (
+    <Box>
+      This is Dashboard
+      <Button color="secondary" onClick={changeMode} variant="outlined">
+        change Mode
+      </Button>
+    </Box>
+  );
 };
 
 export default Dashboard;
