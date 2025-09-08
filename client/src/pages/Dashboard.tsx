5d320169dfd048c7f43b4d11de3a7ab988acae0b
s3nsh1-dev <shubham228pandey@gmail.com>
2025-09-08 19:28:49 +0530

    Sidebar icons corrected

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -1,14 +1,10 @@
 import { Box } from "@mui/material";
 import Test from "./Test";
-import ButtonOne from "../components/ui-components/ButtonOne";
-import ButtonTwo from "../components/ui-components/ButtonTwo";
+
 const Dashboard = () => {
   return (
     <Box>
-      <Test navTitle="">
-        <ButtonOne text="Button One" />
-        <ButtonTwo text="Button two" />
-      </Test>
+      <Test navTitle=""></Test>
     </Box>
   );
 };
