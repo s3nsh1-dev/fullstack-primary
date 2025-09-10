aa5562a67a65273d3767fd9e3e8f7ebd9ccadc5a
s3nsh1-dev <shubham228pandey@gmail.com>
2025-09-10 07:33:54 +0530

    Navbar and Content alligned
    cool

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,28 +1,34 @@
 import Dashboard from "./pages/Dashboard";
 import Navbar from "./pages/Navbar";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { CssBaseline, ThemeProvider } from "@mui/material";
+import { Box, CssBaseline, ThemeProvider } from "@mui/material";
 import { getTheme } from "./utilities/muiThemeController";
 import useMode from "./hooks/useMode";
 import Test from "./pages/Test";
+import { useTheme } from "@mui/material/styles";
+import useMediaQuery from "@mui/material/useMediaQuery";
 
 function App() {
   const { mode } = useMode();
+  const theme = useTheme();
+  const mainSizing = useMediaQuery(theme.breakpoints.down(1313));
 
   return (
-    <ThemeProvider theme={getTheme(mode)}>
-      <CssBaseline />
-      <BrowserRouter>
-        <Routes>
-          {/* routes with Navbar */}
-          <Route element={<Navbar navTitle="" />}>
-            <Route path="/" element={<Dashboard />} />
-          </Route>
-          {/* routes without Navbar */}
-          <Route path="/test" element={<Test />} />
-        </Routes>
-      </BrowserRouter>
-    </ThemeProvider>
+    <Box sx={{ marginTop: mainSizing ? "27px" : "67px" }}>
+      <ThemeProvider theme={getTheme(mode)}>
+        <CssBaseline />
+        <BrowserRouter>
+          <Routes>
+            {/* routes with Navbar */}
+            <Route element={<Navbar navTitle="" />}>
+              <Route path="/" element={<Dashboard />} />
+            </Route>
+            {/* routes without Navbar */}
+            <Route path="/test" element={<Test />} />
+          </Routes>
+        </BrowserRouter>
+      </ThemeProvider>
+    </Box>
   );
 }
 
