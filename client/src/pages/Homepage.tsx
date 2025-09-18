863023ef4c0ac07678b1cf503c512840a79a34eb
s3nsh1-dev <shubham228pandey@gmail.com>
2025-09-18 20:38:47 +0530

    bsdr

--- client/src/pages/Homepage.tsx
+++ client/src/pages/Homepage.tsx
@@ -1,9 +1,56 @@
+import { Box } from "@mui/material";
+
+const emptyPageText: {
+  id: number;
+  logo: string;
+  title: string;
+  heading: string;
+  description: string;
+}[] = [
+  {
+    id: 0,
+    logo: "",
+    title: "",
+    heading: "No videos uploaded",
+    description:
+      "This page has yet to upload a video. Search another page in order to find more videos.",
+  },
+  {
+    id: 1,
+    logo: "",
+    title: "",
+    heading: "No playlist created",
+    description: "There are no playlist created on this channel.",
+  },
+  {
+    id: 2,
+    logo: "",
+    title: "",
+    heading: "No Tweets",
+    description: "This channel has yet to make a Tweet.",
+  },
+  {
+    id: 3,
+    logo: "",
+    title: "",
+    heading: "No people subscribers",
+    description: "This channel has yet to subscribe a new channel.",
+  },
+];
+
 const Homepage = () => {
+  console.log(emptyPageText);
   return (
-    <div>
-      This is user account Homepage showing user videos and other contents with
-      list of subscribers
-    </div>
+    <Box>
+      <Box className="hero-section">
+        <Box className="cover-image"></Box>
+        <Box className="Avatar"></Box>
+      </Box>
+      <Box className="dynamic-content">
+        <Box className="select-button"></Box>
+        <Box className="select-content"></Box>
+      </Box>
+    </Box>
   );
 };
 
