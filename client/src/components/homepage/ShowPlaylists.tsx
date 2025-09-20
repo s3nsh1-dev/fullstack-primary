9b7f1ef3bf489ef24929aa6bebcebca0717e1320
s3nsh1-dev <shubham228pandey@gmail.com>
2025-09-20 20:36:40 +0530

    Subs, playlist and tweets are designed

--- client/src/components/homepage/ShowPlaylists.tsx
+++ client/src/components/homepage/ShowPlaylists.tsx
@@ -1,8 +1,80 @@
 import type { PlaylistType } from "../../constants/dataTypes";
+import {
+  Card,
+  CardContent,
+  Typography,
+  Stack,
+  Box,
+  Avatar,
+} from "@mui/material";
+import convertISOIntoLocalTime from "../../utilities/convertISOIntoLocalTime";
 
 const ShowPlaylists = ({ playlists }: { playlists: PlaylistType[] }) => {
-  console.log("playlists", playlists);
-  return <div>Show user Playlists</div>;
+  if (!playlists || playlists.length === 0) {
+    return <Typography color="textSecondary">No playlists to show.</Typography>;
+  }
+
+  return (
+    <Stack spacing={1} m={1}>
+      {playlists.map((playlist) => (
+        <Card key={playlist._id} variant="outlined">
+          <CardContent sx={{ p: 2 }}>
+            {/* Playlist Info */}
+            <Typography variant="h6" color="textPrimary" gutterBottom>
+              {playlist.name}
+            </Typography>
+            <Typography variant="body2" color="textSecondary" gutterBottom>
+              {playlist.description}
+            </Typography>
+            <Typography
+              variant="caption"
+              color="textSecondary"
+              display="block"
+              mb={2}
+            >
+              Created{" "}
+              {convertISOIntoLocalTime(playlist.createdAt).toLocaleString()}
+            </Typography>
+
+            {/* Videos List */}
+            <Stack direction="row" flexWrap="wrap" spacing={2}>
+              {playlist.videos.map((video) => (
+                <Card
+                  key={video._id}
+                  variant="outlined"
+                  sx={{
+                    display: "flex",
+                    p: 1,
+                    width: 250,
+                    alignItems: "center",
+                  }}
+                >
+                  <Avatar
+                    variant="square"
+                    src={video.thumbnail}
+                    alt={video.title}
+                    sx={{ width: 80, height: 50, mr: 1 }}
+                  />
+                  <Box>
+                    <Typography variant="subtitle2" noWrap>
+                      {video.title}
+                    </Typography>
+                    <Typography
+                      variant="caption"
+                      color="textSecondary"
+                      display="block"
+                    >
+                      {video.duration.toFixed(2)} min | {video.views} views
+                    </Typography>
+                  </Box>
+                </Card>
+              ))}
+            </Stack>
+          </CardContent>
+        </Card>
+      ))}
+    </Stack>
+  );
 };
 
 export default ShowPlaylists;
--- client/src/components/homepage/ShowTweets.tsx
+++ client/src/components/homepage/ShowTweets.tsx
@@ -1,8 +1,47 @@
 import type { TweetType } from "../../constants/dataTypes";
+import { Card, CardContent, Typography, Stack } from "@mui/material";
+import convertISOIntoLocalTime from "../../utilities/convertISOIntoLocalTime";
+import TwitterIcon from "@mui/icons-material/Twitter";
 
 const ShowTweets = ({ tweets }: { tweets: TweetType[] }) => {
-  console.log("tweets", tweets);
-  return <div>Showing User Tweets</div>;
+  if (!tweets || tweets.length === 0) {
+    return <Typography color="textSecondary">No tweets to show.</Typography>;
+  }
+
+  return (
+    <Stack spacing={1} m={1}>
+      {tweets.map((tweet) => (
+        <Card key={tweet._id} variant="outlined" sx={{ borderRadius: "10px" }}>
+          <CardContent
+            sx={{
+              p: 2, // shorthand for padding: 0
+              "&:last-child": {
+                pb: 2, // remove bottom padding
+              },
+            }}
+          >
+            <Typography variant="body1" color="textPrimary">
+              {tweet.content}
+            </Typography>
+            <Typography
+              variant="caption"
+              color="textSecondary"
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                gap: 1,
+                justifyContent: "start",
+                textAlign: "center",
+              }}
+            >
+              <TwitterIcon color="primary" fontSize="small" />
+              {convertISOIntoLocalTime(tweet.createdAt)}
+            </Typography>
+          </CardContent>
+        </Card>
+      ))}
+    </Stack>
+  );
 };
 
 export default ShowTweets;
