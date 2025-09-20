a6efe24db0e3e66e10909f4617c2ce92f64ed4ef
s3nsh1-dev <shubham228pandey@gmail.com>
2025-09-20 17:24:38 +0530

    subs, videos, tweets created

--- server/src/controllers/homepage.controller.ts
+++ server/src/controllers/homepage.controller.ts
@@ -4,42 +4,18 @@ import { User } from "../models/user.model";
 import { asyncHandler } from "../utils/asyncHandler";
 import { isValidObjectId } from "mongoose";
 import { toObjectId } from "../utils/convertToObjectId";
+import { subscribe } from "diagnostics_channel";
 
 const getDetailsForHomepage = asyncHandler(async (req, res) => {
   const { user_ID } = req.params;
   if (!isValidObjectId(user_ID)) throw new ApiError(400, "INVALID USER_ID");
 
-  //   const user = await User.aggregate([
-  //     { $match: { _id: toObjectId(user_ID) } },
-  //     {
-  //       $lookup: {
-  //         from: "subscriptions",
-  //         let: { userId: "$_id" },
-  //         pipeline: [
-  //           { $match: { $expr: { $eq: ["$channel", "$$userId"] } } },
-  //           { $project: { _id: 1, subscriber: 1 } },
-  //         ],
-  //         as: "subscribers_info",
-  //       },
-  //     },
-  //     {
-  //       $project: {
-  //         _id: 1,
-  //         username: 1,
-  //         email: 1,
-  //         fullname: 1,
-  //         avatar: 1,
-  //         coverImage: 1,
-  //         createdAt: 1,
-  //         subscribers_info: 1,
-  //       },
-  //     },
-  //   ]);
   const user = await User.aggregate([
     { $match: { _id: toObjectId(user_ID) } },
     {
       $lookup: {
         from: "subscriptions",
+        as: "subscribers",
         let: { userId: "$_id" },
         pipeline: [
           // RULE: can not user userId without $expr
@@ -47,29 +23,83 @@ const getDetailsForHomepage = asyncHandler(async (req, res) => {
           {
             $lookup: {
               from: "users",
+              as: "subscribedUserInformation",
               let: { subscriberId: "$subscriber" },
               pipeline: [
                 { $match: { $expr: { $eq: ["$_id", "$$subscriberId"] } } },
-                { $project: { _id: 1, avatar: 1, fullname: 1, username: 1 } },
+                { $project: { avatar: 1, fullname: 1, username: 1 } },
               ],
-              as: "subscriber_info",
             },
           },
-          { $unwind: "$subscriber_info" },
+          { $unwind: "$subscribedUserInformation" }, // array to object
           {
             $addFields: {
-              subscriber: "$subscriber_info._id",
-              avatar: "$subscriber_info.avatar",
-              fullname: "$subscriber_info.fullname",
-              username: "$subscriber_info.username",
+              subscriber_id: "$subscriber",
+              avatar: "$subscribedUserInformation.avatar",
+              fullname: "$subscribedUserInformation.fullname",
+              username: "$subscribedUserInformation.username",
+              subscribedAt: "$updatedAt",
             },
           },
-          { $project: { id: 1, avatar: 1, fullname: 1, username: 1 } },
+          {
+            // $project forcefully includes only those fields who are mentioned in addFields when preparing subscribers
+            $project: {
+              subscriber_id: 1,
+              avatar: 1,
+              fullname: 1,
+              username: 1,
+              subscribedAt: 1,
+            },
+          },
         ],
-        as: "subscribers_info",
       },
     },
     {
+      $lookup: {
+        from: "videos",
+        as: "videos",
+        let: { userId: "$_id" },
+        pipeline: [
+          { $match: { $expr: { $eq: ["$owner", "$$userId"] } } },
+          {
+            $project: {
+              title: 1,
+              description: 1,
+              thumbnail: 1,
+              videoFile: 1,
+              duration: 1,
+              views: 1,
+              isPublished: 1,
+              createdAt: 1,
+            },
+          },
+        ],
+      },
+    },
+    {
+      $lookup: {
+        from: "tweets",
+        as: "tweets",
+        let: { userId: "$_id" },
+        pipeline: [
+          { $match: { $expr: { $eq: ["$owner", "$$userId"] } } },
+          { $project: { content: 1, createdAt: 1 } },
+        ],
+      },
+    },
+    {
+      $lookup: {
+        from: "playlists",
+        as: "playlists",
+        let: { userId: "$_id" },
+        pipeline: [
+          { $match: { $expr: { $eq: ["$owner", "$$userId"] } } },
+          { $project: { name: 1, description: 1, videos: 1, createdAt: 1 } },
+        ],
+      },
+    },
+    {
+      // final API look
       $project: {
         _id: 1,
         username: 1,
@@ -78,7 +108,10 @@ const getDetailsForHomepage = asyncHandler(async (req, res) => {
         avatar: 1,
         coverImage: 1,
         createdAt: 1,
-        subscribers_info: 1,
+        subscribers: 1,
+        videos: 1,
+        tweets: 1,
+        playlists: 1,
       },
     },
   ]);
