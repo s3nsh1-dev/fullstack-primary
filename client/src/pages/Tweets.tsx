203c7ec57942a1bc2da64fca68fcc29ed87cbec9
s3nsh1-dev <shubham228pandey@gmail.com>
2025-09-23 12:04:34 +0530

    SOD 23 sept

--- client/src/pages/Tweets.tsx
+++ client/src/pages/Tweets.tsx
@@ -10,7 +10,7 @@ const Tweets = () => {
   const { data, isLoading, isError } = useFetchUserTweets(
     user?.user?._id || ""
   );
-  if (isLoading || !data) return <div>...Loading Liked Content</div>;
+  if (isLoading || !data) return <div>...Loading Tweets</div>;
   if (isError) return <div>...Encountered Error</div>;
 
   // console.log(data);
