8fdfca530b5efda9dcadfb74917eec4435ac7114
s3nsh1-dev <shubham228pandey@gmail.com>
2025-09-26 23:50:17 +0530

    some basic cleanup

--- client/src/pages/Settings.tsx
+++ client/src/pages/Settings.tsx
@@ -1,10 +1,6 @@
 // import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
-import NotLoggedIn from "./NotLoggedIn";
-import useAuth from "../hooks/useAuth";
 
 const Settings = () => {
-  const { user } = useAuth();
-  if (!user) return <NotLoggedIn />;
   return <div>Settings for idk what</div>;
 };
 
