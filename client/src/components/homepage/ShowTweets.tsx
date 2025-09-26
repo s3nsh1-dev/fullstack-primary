357d82698f89fafd769e7c0d1e9c5c9406a0e8ff
s3nsh1-dev <shubham228pandey@gmail.com>
2025-09-26 23:43:37 +0530

    !data is not problem any more after hydrating via isSetteled

--- client/src/components/homepage/ShowTweets.tsx
+++ client/src/components/homepage/ShowTweets.tsx
@@ -1,30 +1,8 @@
 import type { TweetType } from "../../hooks/data-fetching/useFetchUserTweets";
 import { Stack } from "@mui/material";
 import IndividualTweet from "../Tweets/IndividualTweet";
-import useAuth from "../../hooks/useAuth";
-import NotLoggedIn from "../../pages/NotLoggedIn";
 
-const ShowTweets = ({
-  data,
-  isError,
-  isLoading,
-  isFetched,
-  interaction,
-}: {
-  data: TweetType[];
-  interaction: boolean;
-  isError: boolean;
-  isLoading: boolean;
-  isFetched: boolean;
-}) => {
-  const { user, loading } = useAuth();
-
-  // While auth is still hydrating (prevents flash)
-  if (!user && !loading) return <NotLoggedIn />;
-  if (isLoading) return <div>...Loading Tweets</div>;
-  if (isError) return <div>...Encountered Error</div>;
-  if (!data && isFetched) return <div>No Tweets</div>;
-
+const ShowTweets: React.FC<ShowTweetsProps> = ({ data, interaction }) => {
   const renderTweets = data.map((tweet) => (
     <IndividualTweet key={tweet._id} tweet={tweet} interaction={interaction} />
   ));
@@ -33,3 +11,8 @@ const ShowTweets = ({
 };
 
 export default ShowTweets;
+
+type ShowTweetsProps = {
+  data: TweetType[];
+  interaction: boolean;
+};
--- client/src/pages/Tweets.tsx
+++ client/src/pages/Tweets.tsx
@@ -1,22 +1,18 @@
 import useAuth from "../hooks/useAuth";
 import useFetchUserTweets from "../hooks/data-fetching/useFetchUserTweets";
 import ShowTweets from "../components/homepage/ShowTweets";
-import { Box, CircularProgress } from "@mui/material";
+import { Box } from "@mui/material";
 import ChatBubbleOutlineIcon from "@mui/icons-material/ChatBubbleOutline";
 import HomeTabTitles from "../components/ui-components/HomeTabTitles";
 
 const Tweets = () => {
-  const { user, loading } = useAuth();
-  const { data, isError, isLoading, isFetched } = useFetchUserTweets(
+  const { user } = useAuth();
+  const { data, isError, isLoading } = useFetchUserTweets(
     user?.user?._id || ""
   );
-
-  if (loading)
-    return (
-      <div>
-        <CircularProgress />;
-      </div>
-    );
+  if (isLoading) return <div>...Loading Tweets</div>;
+  if (isError) return <div>...Encountered Error</div>;
+  if (!data || data.length === 0) return <div>No Tweets</div>;
 
   return (
     <Box m={1}>
@@ -26,13 +22,7 @@ const Tweets = () => {
           <ChatBubbleOutlineIcon sx={{ fontSize: 28, color: "primary.main" }} />
         }
       />
-      <ShowTweets
-        data={data || []}
-        isError={isError}
-        isLoading={isLoading}
-        isFetched={isFetched}
-        interaction={true}
-      />
+      <ShowTweets data={data} interaction={true} />
     </Box>
   );
 };
