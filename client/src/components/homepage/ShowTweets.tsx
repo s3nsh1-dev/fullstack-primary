133dfd0a02427e04f94685dc136a1e9af041923c
s3nsh1-dev <shubham228pandey@gmail.com>
2025-09-26 22:47:40 +0530

    i think loading issue solved

--- client/src/components/homepage/ShowTweets.tsx
+++ client/src/components/homepage/ShowTweets.tsx
@@ -1,15 +1,31 @@
 import type { TweetType } from "../../hooks/data-fetching/useFetchUserTweets";
 import { Stack } from "@mui/material";
 import IndividualTweet from "../Tweets/IndividualTweet";
+import useAuth from "../../hooks/useAuth";
+import NotLoggedIn from "../../pages/NotLoggedIn";
 
 const ShowTweets = ({
-  tweets,
+  data,
+  isError,
+  isLoading,
+  isFetched,
   interaction,
 }: {
-  tweets: TweetType[];
+  data: TweetType[];
   interaction: boolean;
+  isError: boolean;
+  isLoading: boolean;
+  isFetched: boolean;
 }) => {
-  const renderTweets = tweets.map((tweet) => (
+  const { user, loading } = useAuth();
+
+  // While auth is still hydrating (prevents flash)
+  if (!user && !loading) return <NotLoggedIn />;
+  if (isLoading) return <div>...Loading Tweets</div>;
+  if (isError) return <div>...Encountered Error</div>;
+  if (!data && isFetched) return <div>No Tweets</div>;
+
+  const renderTweets = data.map((tweet) => (
     <IndividualTweet key={tweet._id} tweet={tweet} interaction={interaction} />
   ));
 
