a5bb7746d075e512027dda22e20baa1e10cf7a40
s3nsh1-dev <shubham228pandey@gmail.com>
2025-09-30 00:47:36 +0530

    hooks ready

--- client/src/hooks/data-fetching/useUpdateComment.ts
+++ client/src/hooks/data-fetching/useUpdateComment.ts
@@ -6,13 +6,13 @@ const useUpdateComment = () => {
   return useMutation({
     mutationKey: ["updateComment"],
     mutationFn: async ({ commentId, content }: UpdateCommentInputType) => {
-      const response = await fetch(`${URL}/comments/c/${commentId}`, {
+      const response = await fetch(`${URL}/comments/modify/${commentId}`, {
         method: "PATCH",
         credentials: "include",
         body: JSON.stringify({ content }),
       });
       if (!response.ok) throw new Error("ERROR WHILE UPDATING COMMENT");
-      const result = await response.json();
+      const result: UpdateCommentResponse = await response.json();
       return result;
     },
   });
@@ -24,3 +24,31 @@ type UpdateCommentInputType = {
   commentId: string;
   content: string;
 };
+
+export type UpdateCommentResponse = {
+  statusCode: number;
+  data: {
+    result: Result;
+  };
+  message: string;
+  success: boolean;
+};
+
+type BaseResult = {
+  _id: string;
+  content: string;
+  owner: {
+    _id: string;
+    fullname: string;
+    avatar: string;
+  };
+  createdAt: string; // ISO
+  updatedAt: string; // ISO
+  __v: number;
+};
+
+// Union for the reference type makes sure only one type is allowed
+type Result =
+  | (BaseResult & { comment: string })
+  | (BaseResult & { tweet: string })
+  | (BaseResult & { video: string });
