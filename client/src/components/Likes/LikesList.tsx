93eaf3a1154c98bc10bd66762917cc449334a3cb
s3nsh1-dev <shubham228pandey@gmail.com>
2025-10-04 00:05:07 +0530

    delegation i progress

--- client/src/components/Likes/LikesList.tsx
+++ client/src/components/Likes/LikesList.tsx
@@ -5,71 +5,14 @@ import type {
   Comment,
   Tweet,
 } from "../../hooks/data-fetching/useFetchLikedContent";
-import {
-  Box,
-  Card,
-  Typography,
-  CardContent,
-  Avatar,
-  Divider,
-  IconButton,
-} from "@mui/material";
-import ContentProfileHeader from "../Tweets/ContentProfileHeader";
-import useAuth from "../../hooks/useAuth";
-import convertISOIntoLocalTime from "../../utilities/convertISOIntoLocalTime";
-import ThumbUpIcon from "@mui/icons-material/ThumbUp";
+import { Box, Card } from "@mui/material";
+import ShowMyLikesOnTweet from "./ShowMyLikesOnTweet";
 
 const LikesList: React.FC<LikesListProps> = ({ data }) => {
-  const { user } = useAuth();
   const renderAll = data.map((item) => {
     if (isLikeTweet(item)) {
       // console.log("tweet", item.tweet);
-      return (
-        <Card key={item._id} sx={style1} elevation={4}>
-          <Box
-            sx={{
-              display: "flex",
-              alignItems: "center",
-            }}
-          >
-            <ContentProfileHeader
-              style2={{}}
-              imgSrc={item.tweet.owner.avatar}
-              fullname={item.tweet.owner.fullname}
-              username={item.tweet.owner.username}
-              createdAt={item.tweet.updatedAt}
-            />
-            <Divider orientation="vertical" flexItem sx={{ mx: 2 }} />
-            <Box>
-              <Box sx={{ display: "flex", alignItems: "center" }}>
-                <IconButton>
-                  <ThumbUpIcon fontSize="small" color="primary" />
-                </IconButton>
-                <Typography
-                  sx={{ mr: 1 }}
-                  variant="caption"
-                  color="textSecondary"
-                >
-                  by
-                </Typography>
-                <Avatar
-                  src={user?.user?.avatar}
-                  alt="my-avatar"
-                  sx={{ width: 25, height: 25 }}
-                />
-              </Box>
-              <Typography variant="caption" color="textSecondary">
-                {convertISOIntoLocalTime(item.updatedAt)}
-              </Typography>
-            </Box>
-          </Box>
-          <CardContent sx={style5}>
-            <Typography variant="body1" color="textPrimary" sx={style6}>
-              {item.tweet.content}
-            </Typography>
-          </CardContent>
-        </Card>
-      );
+      return <ShowMyLikesOnTweet key={item._id} item={item} />;
     }
     if (isLikeVideo(item)) {
       // console.log("video", item.video);
@@ -107,13 +50,3 @@ const isLikeComment = (
 ): item is LikedItem & { comment: Comment } => {
   return item.comment !== undefined;
 };
-
-const style1 = { padding: "10px" };
-
-const style5 = {
-  p: 0, // shorthand for padding: 0
-  "&:last-child": {
-    pb: 0, // remove bottom padding
-  },
-};
-const style6 = { mt: "5px" };
---
+++ client/src/components/Likes/ShowMyLikesOnVideo.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ShowMyLikesOnVideo = () => {
+  return <div></div>;
+};
+
+export default ShowMyLikesOnVideo;
