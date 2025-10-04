643476a0bd6fe62c814ad25c0784524de1c5f377
s3nsh1-dev <shubham228pandey@gmail.com>
2025-10-04 12:57:09 +0530

    SOD 4oct

--- client/src/components/Likes/LikesList.tsx
+++ client/src/components/Likes/LikesList.tsx
@@ -6,27 +6,19 @@ import type {
   Tweet,
 } from "../../hooks/data-fetching/useFetchLikedContent";
 import ShowMyLikesOnTweet from "./ShowMyLikesOnTweet";
-import { Box, Card } from "@mui/material";
+import { Box } from "@mui/material";
 import ShowMyLikesOnComment from "./ShowMyLikesOnComment";
-// import { CardContent, Typography } from "@mui/material";
-// import ContentProfileHeader from "../Tweets/ContentProfileHeader";
-// import ShowLikeOwner from "./ShowLikeOwner";
-// import CommentIcon from "@mui/icons-material/Comment";
+import ShowMyLikesOnVideo from "./ShowMyLikesOnVideo";
 
 const LikesList: React.FC<LikesListProps> = ({ data }) => {
   const renderAll = data.map((item) => {
-    if (isLikeTweet(item)) {
+    const link = item.comment?.tweet ?? item.comment?.video;
+    if (isLikeTweet(item))
       return <ShowMyLikesOnTweet key={item._id} item={item} />;
-    }
-    if (isLikeVideo(item)) {
-      console.log("video", item.video);
-      return <Card key={item._id} sx={style1} elevation={4}></Card>;
-    }
-    if (isLikeComment(item)) {
-      // console.log("comment", item.comment);
-      const link = item.comment.tweet ?? item.comment.video;
+    if (isLikeVideo(item))
+      return <ShowMyLikesOnVideo key={item._id} item={item} />;
+    if (isLikeComment(item))
       return <ShowMyLikesOnComment key={item._id} item={item} link={link} />;
-    }
   });
 
   return (
@@ -55,15 +47,3 @@ const isLikeComment = (
 ): item is LikedItem & { comment: Comment } => {
   return item.comment !== undefined;
 };
-
-const style1 = { padding: "10px" };
-
-// const style5 = {
-//   p: 0, // shorthand for padding: 0
-//   "&:last-child": {
-//     pb: 0, // remove bottom padding
-//   },
-//   display: "flex",
-//   alignItems: "center",
-// };
-// const style6 = { mt: "5px", overflow: "hidden", textOverflow: "ellipsis" };
