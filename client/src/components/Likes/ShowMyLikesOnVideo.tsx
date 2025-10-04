4772eb9fcf32935660fadb7a2c5c432d7841ac75
s3nsh1-dev <shubham228pandey@gmail.com>
2025-10-04 12:57:09 +0530

    SOD 4oct

--- client/src/components/Likes/ShowMyLikesOnVideo.tsx
+++ client/src/components/Likes/ShowMyLikesOnVideo.tsx
@@ -1,7 +1,25 @@
 import React from "react";
-
-const ShowMyLikesOnVideo = () => {
-  return <div></div>;
+import { Card } from "@mui/material";
+import type { LikedItem } from "../../hooks/data-fetching/useFetchLikedContent";
+const ShowMyLikesOnVideo: React.FC<ShowMyLikesOnVideoProps> = ({ item }) => {
+  console.log("video", item.video);
+  return <Card sx={style1} elevation={4}></Card>;
 };
 
 export default ShowMyLikesOnVideo;
+
+type ShowMyLikesOnVideoProps = {
+  item: LikedItem;
+};
+
+const style1 = { padding: "10px" };
+
+// const style5 = {
+//   p: 0, // shorthand for padding: 0
+//   "&:last-child": {
+//     pb: 0, // remove bottom padding
+//   },
+//   display: "flex",
+//   alignItems: "center",
+// };
+// const style6 = { mt: "5px", overflow: "hidden", textOverflow: "ellipsis" };
