e0739221fac021108ae5771541f1e61d0fbc4b0b
s3nsh1-dev <shubham228pandey@gmail.com>
2025-10-23 23:22:30 +0530

    now search User is broken

--- server/src/controllers/tweet.controller.ts
+++ server/src/controllers/tweet.controller.ts
@@ -154,14 +154,60 @@ const getUserTweets = asyncHandler(async (req, res) => {
   const { userId } = req.params;
   if (!isValidObjectId(userId)) throw new ApiError(400, "INVALID USER_ID");
 
-  const tweets = await Tweet.find({ owner: toObjectId(userId) }).populate({
-    path: "owner",
-    select: "username fullname avatar",
-  });
+  const foo = await Tweet.aggregate([
+    { $match: { owner: toObjectId(userId) } },
+    {
+      $lookup: {
+        from: "users",
+        localField: "owner",
+        foreignField: "_id",
+        as: "owner",
+        pipeline: [{ $project: { username: 1, fullname: 1, avatar: 1 } }],
+      },
+    },
+    {
+      $lookup: {
+        from: "likes",
+        let: { tweetId: "$_id" }, // this is tweet_id fetched during execution
+        as: "likedDetails",
+        pipeline: [
+          {
+            $match: {
+              $expr: {
+                $and: [
+                  { $eq: ["$tweet", "$$tweetId"] },
+                  { $eq: ["$likedBy", toObjectId(userId)] },
+                ],
+              },
+            },
+          },
+          { $limit: 1 },
+        ],
+      },
+    },
+    {
+      $addFields: {
+        isLiked: {
+          $cond: [{ $gt: [{ $size: "$likedDetails" }, 0] }, true, false],
+        },
+      },
+    },
+
+    { $unwind: { path: "$owner", preserveNullAndEmptyArrays: true } },
+    {
+      $project: {
+        content: 1,
+        owner: 1,
+        createdAt: 1,
+        updatedAt: 1,
+        isLiked: 1,
+      },
+    },
+  ]);
 
   return res
     .status(200)
-    .json(new ApiResponse(200, { tweets }, "USER TWEETS RETRIEVED"));
+    .json(new ApiResponse(200, { tweets: foo }, "USER TWEETS RETRIEVED"));
 });
 
 export { createTweet, getUserTweets, updateTweet, deleteTweet, fetchTweet };
