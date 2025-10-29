758405e6fb1f4e2920e6e108bf81178d51c2d507
s3nsh1-dev <shubham228pandey@gmail.com>
2025-10-29 23:49:39 +0530

    EOD

--- client/src/pages/Settings.tsx
+++ client/src/pages/Settings.tsx
@@ -1,13 +1,11 @@
 import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
 import Box from "@mui/material/Box";
 import HomeTabTitles from "../components/ui-components/HomeTabTitles";
-import ChangeUsername from "../components/setting/ChangeUsername";
 
 const Settings = () => {
   return (
     <Box p={1}>
       <HomeTabTitles text="Settings" icon={<SettingsOutlinedIcon />} />
-      <ChangeUsername />
     </Box>
   );
 };
