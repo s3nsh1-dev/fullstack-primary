88cf248c74706732dc07ef30ab624156869e8c69
s3nsh1-dev <shubham228pandey@gmail.com>
2025-10-31 23:09:57 +0530

    account update hook

--- client/src/pages/Settings.tsx
+++ client/src/pages/Settings.tsx
@@ -1,17 +1,12 @@
 import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
 import Stack from "@mui/material/Stack";
 import HomeTabTitles from "../components/ui-components/HomeTabTitles";
-import ChangeUsername from "../components/setting/ChangeUsername";
-import ChangeUserPassword from "../components/setting/ChangeUserPassword";
 
 const Settings = () => {
   return (
     <Stack p={1} gap={1}>
       <HomeTabTitles text="Settings" icon={<SettingsOutlinedIcon />} />
-      Change username
-      <ChangeUsername />
-      Change password
-      <ChangeUserPassword />
+      Change Fullname and email
     </Stack>
   );
 };
