9052167416f041465cd1625215c57a2ad2236a37
s3nsh1-dev <shubham228pandey@gmail.com>
2025-11-17 23:57:54 +0530

    inprocress for paginating watchHistory

--- server/src/controllers/userControllers/getWatchHistory.ts
+++ server/src/controllers/userControllers/getWatchHistory.ts
@@ -2,6 +2,7 @@ import { User } from "../../models/user.model";
 import { asyncHandler } from "../../utils/asyncHandler";
 import ApiError from "../../utils/ApiError";
 import ApiResponse from "../../utils/ApiResponse";
+import { Video } from "../../models/video.model";
 
 const getWatchHistory = asyncHandler(async (req, res) => {
   //TODO: get watchHistory for a user
@@ -9,20 +10,25 @@ const getWatchHistory = asyncHandler(async (req, res) => {
     throw new ApiError(401, "UNAUTHENTICATED USER");
   }
   const userId = req.user._id;
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
 
-  const userWatchHistory = await User.findById(userId)
-    .select("watchHistory")
-    .populate({
-      path: "watchHistory",
-      select:
-        "owner title description videoFile thumbnail views duration isPublished updatedAt createdAt",
-      populate: { path: "owner", select: "fullname username avatar" },
-    });
-  if (!userWatchHistory) throw new ApiError(404, "User not found");
+  const user = await User.findById(userId).select("watchHistory");
 
-  return res
-    .status(200)
-    .json(new ApiResponse(200, userWatchHistory, "Watch history fetched"));
+  // slice BEFORE populate
+  const start = (page - 1) * limit;
+  const end = start + limit;
+  const slicedIds = user.watchHistory.slice(start, end);
+
+  // populate sliced ids only
+  const videos = await Video.find({ _id: { $in: slicedIds } })
+    .populate("owner", "fullname username avatar")
+    .lean();
+
+  return res.json({
+    videos,
+    total: user.watchHistory.length,
+  });
 });
 
 export { getWatchHistory };
