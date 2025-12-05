f3b7bdd1e7db1cbc186d235fe322f5cadd9907fa
s3nsh1-dev <shubham228pandey@gmail.com>
2025-12-05 23:46:41 +0530

    auth needed

--- client/src/pages/OpenSingleTweetPage.tsx
+++ client/src/pages/OpenSingleTweetPage.tsx
@@ -4,12 +4,17 @@ import { useParams } from "react-router-dom";
 import Box from "@mui/material/Box";
 import ContentNotAvailable from "../components/others/ContentNotAvailable";
 import LoadingAnimation from "../components/ui-components/LoadingAnimation";
+import useAuth from "../hooks/useAuth";
 
 const OpenSingleTweetPage = () => {
   const { tweetId } = useParams<{ tweetId: string }>(); // fetch param from URL
-  const { data, isLoading } = useFetchSingleTweet(
-    tweetId || "INVALID_TWEET-ID"
-  );
+  const { user } = useAuth();
+  console.log(user);
+  const { data, isLoading } = useFetchSingleTweet({
+    tweetId: tweetId || "",
+    userId: user?.user?._id || "",
+  });
+  console.log(data);
   if (isLoading) return <LoadingAnimation />;
   if (!data) return <ContentNotAvailable text="Tweet Not Available" />;
 
